c1f602409d8cd848adb15c9c02a8eae4ccb24e9c
ahmad <nadeem.ahmad2@ibm.com>
2020-06-21 01:52:41 +0500

    Placeholder added if no feedbacks to be rendered.

--- src/components/feedbacks/feedback/feedback.js
+++ src/components/feedbacks/feedback/feedback.js
@@ -4,29 +4,31 @@ import Rater from 'react-star-ratings';
 
 const feedback = (props) => {
     return (
-        props.data.map((feedback, i) => {
-            return (
-                <div className={classes.Feedback} key={i}>
-                    <div className={classes.FeedbackImg}>
-                        <img src={feedback.user.avatar} />
-                    </div>
-                    <div className={classes.User}>
-                        <div className={classes.NameRating}>
-                            <div className={classes.Username}>{feedback.user.name}</div>
-                            <Rater
-                                numberOfStars={5}
-                                rating={feedback.rating}
-                                starRatedColor={'gold'}
-                                starHoverColor={'gold'}
-                                starEmptyColor={'#d5d5d5'}
-                                starDimension={'20px'}
-                                starSpacing={'0px'} />
+        props.data.length === 0 ? <h1 className={classes.NoFeedbacks}>No Feedbacks yet !</h1> :
+
+            props.data.map((feedback, i) => {
+                return (
+                    <div className={classes.Feedback} key={i}>
+                        <div className={classes.FeedbackImg}>
+                            <img src={feedback.user.avatar} />
                         </div>
-                        <div className={classes.Comment}>{feedback.comment}</div>
+                        <div className={classes.User}>
+                            <div className={classes.NameRating}>
+                                <div className={classes.Username}>{feedback.user.name}</div>
+                                <Rater
+                                    numberOfStars={5}
+                                    rating={feedback.rating}
+                                    starRatedColor={'gold'}
+                                    starHoverColor={'gold'}
+                                    starEmptyColor={'#d5d5d5'}
+                                    starDimension={'20px'}
+                                    starSpacing={'0px'} />
+                            </div>
+                            <div className={classes.Comment}>{feedback.comment}</div>
+                        </div>
                     </div>
-                </div>
-            )
-        })
+                )
+            })
 
     )
 }
